db559b34bab317d3f691e20947f26c8698dc390b
Jdwashin9 <joshuawashington2022@gmail.com>
2025-03-31 13:58:59 +0000

    Tests from 4A Checkpoint copied 0956

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,4 +1,5 @@
 from src.translator import translate_content
+from mock import patch, MagicMock
 
 
 def test_chinese():
@@ -11,3 +12,38 @@ def test_llm_normal_response():
 
 def test_llm_gibberish_response():
     pass
+
+@patch.object(client.chat.completions, 'create')
+def test_unexpected_language(mocker):
+  # we mock the model's response to return a random message
+
+  # mock return bad language results in terms of length
+  mocker.return_value.choices[0].message.content = "I don't understand your request"
+  assert query_llm_robust("Hier ist dein erstes Beispiel.")==(False, "Sorry, a language detection and translation was run on your post, but due to some error, the language result contain more information than needed.")
+
+  # mock return bad language results in terms of return type
+  mocker.return_value.choices[0].message.content = 0
+  assert query_llm_robust("Il fait beau aujourd'hui.")==(False, "Sorry, a language detection and translation was run on your post, but due to some error, the language result returned something that is not a string.")
+
+  # mock if errored in any OpenAI api calls
+  mocker.side_effect = OpenAIError("OpenAI Error")
+  assert query_llm_robust("我要去上课。")==(False, "Sorry, a language detection and translation was run on your post, but due to some error, the calls failed.")
+
+  # mock if encounter any error other than OpenAI errors
+  mocker.side_effect = Exception("Some Error")
+  assert query_llm_robust("À quelle heure part le train?")==(False, "Sorry, a language detection and translation was run on your post, but due to some error, the processes did not return valid response.")
+
+  # mock return bad translation results in terms of return type
+  first_mock = MagicMock()
+  first_mock.choices = [MagicMock()]
+  first_mock.choices[0].message = MagicMock()
+  first_mock.choices[0].message.content = "Chinese"
+
+  second_mock = MagicMock()
+  second_mock.choices = [MagicMock()]
+  second_mock.choices[0].message = MagicMock()
+  second_mock.choices[0].message.content = 0
+
+  mocker.side_effect = [first_mock, second_mock]
+  assert query_llm_robust("C'est un exemple de message.")==(False, "Sorry, a language detection and translation was run on your post, but due to some error, the translation result returned something that is not a string.")
+
