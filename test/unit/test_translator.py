488901f10f9df403bbccb7540daff489726cd39c
VallinZ <107499985+VallinZ@users.noreply.github.com>
2025-04-06 18:26:30 -0400

    Small changes to run the tests

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -11,9 +11,9 @@ def test_llm_normal_response():
     is_english, translated_content = translate_content("This is a test for normal response.")
     assert is_english == True
     assert translated_content == "This is a test for normal response."
-    is_english, translated_content = translate_content("Ich liebe es, mit Python zu programmieren, weil es einfach und gleichzeitig leistungsfähig ist.")
-    assert is_english == False
-    assert translated_content == "I love programming with Python because it's simple yet powerful."
+    is_english1, translated_content1 = translate_content("Ich liebe es, mit Python zu programmieren, weil es einfach und gleichzeitig leistungsfähig ist.")
+    assert is_english1 == False
+    assert translated_content1 == "I love programming with Python because it's simple yet powerful."
 
 def test_llm_gibberish_response():
     is_english, translated_content = translate_content("asdgkjasndgo;98i43qwtoishfjn")
